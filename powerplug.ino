16625d4fc3abd5fcb58d218dacac479168bfa7f7
Radoslaw Biernacki <radoslaw.biernacki@gmail.com>
2019-06-04 09:53:41 +0200

    Wroclaw presentation

--- powerplug.ino
+++ powerplug.ino
@@ -11,21 +11,14 @@ I'll be pleased if you'd do it by sharing http://youtube.com/bitlunislab
 
 const int RF_OUT = 2; //PIN for amplitude modulation
 
-const char *on_short  = "1011000110111010101011000";
-const char *off_short = "1011111011100001001011000";
-const char *on_long  = "011011111011001001101000110100110";
-const char *off_long = "011111110011010001101101100100110";
+const char *on_short   = "1011111110101100010111000";
+const char *off_short  = "1011001101101000100011000";
 
 const char *ir_arm = "0100011000011001100000010";
 const char *ir_dis = "0100011000011001100000100";
 
 
 #define TXSPEED 115200
-
-/* Use short code for powerplug */
-#define SHORT_CODE
-/* Use power plug */
-#define POWER_PLUG
 
 void setup() 
 {
@@ -37,35 +30,21 @@ void loop()
 {
   int i;
 
-#ifdef POWER_PLUG
   Serial.println("On");
-#ifdef SHORT_CODE
+
   for (i = 0; i < 4; i++) {
     rfWriteString(RF_OUT, 382, on_short, true);
     delayMicroseconds(382 * 8);
   }
-#else
-  for (i = 0; i < 4; i++) {
-    rfWriteString(RF_OUT, 510, on_long, false);
-    delayMicroseconds(510 * 8);
-  }
-#endif
 
   delay(3000);
 
   Serial.println("Off");
-#ifdef SHORT_CODE
+
   for (i = 0; i < 4; i++) {
     rfWriteString(RF_OUT, 382, off_short, true);
     delayMicroseconds(382 * 8);
   }
-#else
-  for (i = 0; i < 4; i++) {
-    rfWriteString(RF_OUT, 510, off_long, false);
-    delayMicroseconds(510 * 8);
-  }
-#endif
-#endif
   
   delay(3000);
 
